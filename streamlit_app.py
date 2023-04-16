0e5d56aaae69979a89b7e91808766c9a98ec76ea
Baserri Technologies <125121124+BaserriTechnologies@users.noreply.github.com>
2023-04-16 20:44:00 +0200

    Update streamlit_app.py
    New comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 
 import streamlit
-streamlit.title('Hello Snowflake!')
-streamlit.header('My header text')
-streamlit.text('My text text')
+
+#streamlit.title('Hello Snowflake!')
+#streamlit.header('My header text')
+#streamlit.text('My text text')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
